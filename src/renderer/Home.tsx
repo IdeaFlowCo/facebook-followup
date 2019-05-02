1b6ea357a7b00603f0bafdccae1db02fc702bfe1
Ian Campbell <iancam92@icloud.com>
2019-05-02 13:24:13 -0700

    minor commit

--- src/renderer/Home.tsx
+++ src/renderer/Home.tsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react"; // import { Link } from "react-router-dom";
 import { ipcRenderer } from "electron";
 let styles = require("./Home.css");
+
 type Props = {};
 
 type participant = {
